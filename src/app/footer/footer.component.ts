2e3b8f35e7bbc8ed022559ca1f5407423bd1f6d1
doh6077 <kimdohee@sheridancollege.ca>
2024-07-10 15:52:41 -0400

    Implement FooterComponent to initialize content from route data

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {Content, Projects} from '../interaceCP';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
-  @Input() content!:Content
+export class FooterComponent implements OnInit{
+  content: Content = {
+    name: '',
+    linkedIn: '',
+    role: '',
+    motto: '',
+    image: ''
+  };
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.content = data['content'];
+    });
+  }
 
 }
